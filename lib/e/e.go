7dc32dd8ac95a5cdd2279ad414800841e2232e06
Sargsian Rafik <alex_23kudinov@mail.ru>
2025-10-28 22:39:41 +0700

    feat(bot): добавлена первоначальная структура бота, клиент телеги

--- lib/e/e.go
+++ lib/e/e.go
@@ -5,7 +5,6 @@ import "fmt"
 func Wrap(msg string, err error) error {
 	return fmt.Errorf("can't do request: %w", err)
 }
-
 func WrapIfError(msg string, err error) error {
 	if err == nil {
 		return nil
