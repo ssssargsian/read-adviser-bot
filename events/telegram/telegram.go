5bc5185ac6a1e094f21c396ece786ea2f2b7d8f7
Sargsian Rafik <ssssargsian@gmail.com>
2025-10-31 11:44:24 +0700

    feat(bot): добавил storage для хранения записей и реализованы методы + начали делать event processor

---
+++ events/telegram/telegram.go
@@ -0,0 +1,13 @@
+package telegram
+
+import "read-adviser-bot/clients/telegram"
+
+type Processor struct {
+	tg     *telegram.Client
+	offset int
+	// storage
+}
+
+func New(client *telegram.Client) {
+
+}
